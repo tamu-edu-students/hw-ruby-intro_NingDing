9a35bedec8e2109a6344e4429314e1b17d81ed28
Ubuntu <ubuntu@ip-172-31-19-152.ec2.internal>
2022-09-10 23:50:59 +0000

    csce 606 hw1 code and test result ning ding

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,75 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  
+  arr.sum()
+
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  
+  arr_max_sum = arr.max_by(2){|num| num}
+  arr_max_sum.sum
+  
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  
+  hash = Hash.new()
+  
+  (0..arr.length-1).each do |i|
+    return true if hash.key?(number-arr[i])
+    hash.store(arr[i],i)
+  end
+  
+  return false
+  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  
+  return "Hello, #{name}"
+  
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  
+  return  string =~ /\A(?=[^aeiou])(?=[a-z])/i 
+  
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  
+  return true if string ==  "0"
+  return string.match(/^[01]*00$/) != nil
+  
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  
+	attr_accessor :isbn
+	attr_accessor :price
+	
+  def initialize(isbn, price)
+    
+    if isbn.length == 0  or price <= 0
+      raise ArgumentError.new("Wrong arguments")
+    end
+			
+		@isbn = isbn
+		@price = price
+		
+  end
+	
+	def price_as_string
+	  
+	  return "$%0.2f" % [price]
+	  
+	end  
+  
 end
